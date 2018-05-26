504fbe89dc8cd2a24324aaca0a6f039224779107
HootOwl17 <davidscout2010@hotmail.com>
2018-05-26 09:34:39 -0600

    Codigo Para El Arduino

---
+++ sketch_may25a/sketch_may25a.ino
@@ -0,0 +1,30 @@
+/*
+ T05_Temperatura
+ */
+
+const int sensor = 0;
+const int ledRojo = 5;
+const int ledAzul = 6;
+
+long miliVolts;
+long temperatura;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  miliVolts = (analogRead(sensor) * 5000L) / 1023;
+  temperatura = miliVolts / 10;
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" grados");
+  delay(1000);
+ 
+
+}
+
+
+
