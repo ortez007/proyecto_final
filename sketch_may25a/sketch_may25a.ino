65cc60065f1a9f5799886361d232c9f7c5e10f7d
HootOwl17 <davidscout2010@hotmail.com>
2018-05-26 10:33:43 -0600

    Arreglos al Codigo

--- sketch_may25a/sketch_may25a.ino
+++ sketch_may25a/sketch_may25a.ino
@@ -8,20 +8,32 @@ const int ledAzul = 6;
 
 long miliVolts;
 long temperatura;
+int brillo;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //iniciamos la comunicacion serial
+  pinMode(ledRojo, OUTPUT);
+  pinMode(ledAzul, OUTPUT);
 
 }
 
 void loop() {
-  miliVolts = (analogRead(sensor) * 5000L) / 1023;
-  temperatura = miliVolts / 10;
+  miliVolts = (analogRead(sensor) * 5000L) / 1023; //calculando los mv en la entrada
+  temperatura = miliVolts / 10; //calculamos la temperatura
 
-  Serial.print("Temperatura: ");
+  brillo = map(temperatura, 10, 40, 0, 255); //ajustamos la escala de temperatura para usar analowrite
+  brillo = constrain(brillo, 0, 255);
+
+  analogWrite(ledRojo, brillo);
+  analogWrite(ledAzul, 255 - brillo); //ajustamos el color del led
+  
+
+  
+
+  Serial.print("Temperatura: "); //mandamos la temperatura
   Serial.print(temperatura);
   Serial.println(" grados");
-  delay(1000);
+  delay(500);
  
 
 }
